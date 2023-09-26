c1172ca81b90a90afbe94a29f0c1c8605df3f2f5
ford_ji <ford_ji@epam.com>
2023-09-26 10:23:20 +0800

    add file upload API to test PV & PVC

--- sales-order/src/main/java/com/epam/workshop/sales/product/client/app/ConfigurationPrinterController.java
+++ sales-order/src/main/java/com/epam/workshop/sales/product/client/app/ConfigurationPrinterController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/configuration/print")
+@RequestMapping("/api/v1/order/configuration/print")
 public class ConfigurationPrinterController {
 
     @Autowired
