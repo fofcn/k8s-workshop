9ddc9f308fb69afb4114a46f5ad3be90298845d0
ford_ji <ford_ji@epam.com>
2023-10-17 16:39:03 +0800

    add logstash and MDC testing

--- sales-order/src/main/java/com/epam/workshop/sales/product/client/app/OrderController.java
+++ sales-order/src/main/java/com/epam/workshop/sales/product/client/app/OrderController.java
@@ -2,12 +2,14 @@ package com.epam.workshop.sales.product.client.app;
 
 import com.epam.workshop.sales.product.client.dto.OrderProductNameDto;
 import com.epam.workshop.sales.product.infrastructure.feign.ProductFeignClient;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/order")
 public class OrderController {
@@ -17,6 +19,7 @@ public class OrderController {
 
     @GetMapping("/{orderNo}/product/name")
     public OrderProductNameDto getOrderProductName(@PathVariable("orderNo")String orderNo) {
+        log.info("This is my MDC test");
         String productName = productFeignClient.getProductName(1L);
         return new OrderProductNameDto(productName);
     }
